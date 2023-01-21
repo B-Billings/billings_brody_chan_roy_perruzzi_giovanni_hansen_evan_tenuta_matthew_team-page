a722f5c0c067a9e2da08fa9b692f0a5d6d060d61
Giovanni <delmert38@gmail.com>
2023-01-20 23:44:01 -0500

    Updating base files

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,22 @@
 console.log('test');
-let tl = gsap.timeline({defaults:{ease: "SlowMo.easeIn"}});
-    tl.to(".fillsection",{opacity:'100%',duration:4, stagger:2});
+let tl = gsap.timeline({
+    defaults: {
+        ease: "SlowMo.easeIn"
+    }
+});
+tl.to(".fillsection", {
+    opacity: '100%',
+    duration: 4,
+    stagger: 2
+});
 
-    let tp = gsap.timeline({defaults:{ease: "easeIn"}});
-    tp.to("#logo img",{opacity:'100%',duration:2, stagger:0});
-
-    
+let tp = gsap.timeline({
+    defaults: {
+        ease: "easeIn"
+    }
+});
+tp.to("#logo img", {
+    opacity: '100%',
+    duration: 2,
+    stagger: 0
+});
